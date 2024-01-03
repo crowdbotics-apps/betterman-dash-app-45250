c9647e20943d67fadd9d3685e90743601c8a053d
Crowdbotics <team@crowdbotics.com>
2024-01-03 00:50:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "betterman-dash-app-45250",
+  url: "https://betterman-dash-app-45250.botics.co",
+  api: "https://betterman-dash-app-45250.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
